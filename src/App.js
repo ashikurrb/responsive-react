83bb920bcb4739f96fa126bf673cf9dcc2b9b5d6
Ashikur Rahman Bhuiyan <80683078+ashikurrb@users.noreply.github.com>
2022-04-19 16:39:28 +0600

    changes made to react router dom v5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
 
 function App() {
   return (
     <div className="App">
-     <Home></Home>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+          <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
