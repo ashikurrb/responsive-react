d8840f5b3880ed90a6d2d76b0d778b9e71da7ac8
Ashikur Rahman Bhuiyan <80683078+ashikurrb@users.noreply.github.com>
2022-04-21 12:20:26 +0600

    restart 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home></Home>
           </Route>
           <Route path="/home">
-          <Home></Home>
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
           </Route>
         </Switch>
       </Router>
--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1 className='text-danger'>404!! Not FOUND</h1>
-        </div>
-    );
+  return (
+    <div className="container">
+      <img
+        style={{ width: "75%"}}
+        className="img-fluid"
+        src="https://miro.medium.com/max/1400/1*cLQUX8jM2bMdwMcV2yXWYA.jpeg"
+        alt=""
+      />
+      <br />
+      <br />
+
+      <Link to="/home">
+        <button>GO Back</button>
+      </Link>
+
+    </div>
+
+  );
 };
 
 export default NotFound;
