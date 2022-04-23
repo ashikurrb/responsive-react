dfd2c4e3a7b2461b862df70af80f0831823504be
Ashikur Rahman Bhuiyan <80683078+ashikurrb@users.noreply.github.com>
2022-04-23 10:56:12 +0600

    init

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Service.css'
 
 const Service = ({ service }) => {
   //const {service} = props;
-  const { name, price, description, img } = service;
+  const { id, name, price, description, img } = service;
   return (
-    <div className="service m-4">
+    <div className="service m-4 py-3">
       <img src={img} alt="" />
       <h4>{name}</h4>
       <h3>{price}</h3>
       <p className="p-3">{description}</p>
+    <Link to={`/booking/${id}`}> <button className="btn btn-primary">Book {name}</button></Link>
     </div>
   );
 };
